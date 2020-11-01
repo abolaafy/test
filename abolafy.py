c5920d57de99368cbf6c5183ef2548b1c6b8580f
abolaafy <68032555+abolaafy@users.noreply.github.com>
2020-11-01 16:21:42 +0200

    Update abolafy.py

--- abolafy.py
+++ abolafy.py
@@ -1,7 +1,8 @@
 from pafy import *
 import os
+print ("                          <==============WOLCOME YOU==================>                           ")
 print ("os.chdir("/sdcard")
-vido = input ("ENTER URL any vedio Yuotube=======>
+vido = input ("ENTER URL any vedio Yuotube=======>")
 vid =new(vido)
 down = vid.gatbest()
 down.download()
