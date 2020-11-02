e27246fc589229e86bf86dc912d20d929671c33e
abolaafy <68032555+abolaafy@users.noreply.github.com>
2020-11-02 16:45:05 +0200

    Update abolafy.py

--- abolafy.py
+++ abolafy.py
@@ -1,7 +1,15 @@
 from pafy import *
 import os
-print ("                          <==============WOLCOME YOU==================>                           ")
+r="\033[1;31m"
+print (" <><><><>ABOLAFY<><><><>")
+print (" <==============WOLCOME YOU===========>")
+print ("------------------------------------")
+print ("------------------------------------")
+print ("HACK FACEBOOK XX")
+print ("HACK WHATSAPP XX")
+print ("HACK ANDOROID XX")
 print ("os.chdir("/sdcard")
+print ("######################################")       
 vido = input ("ENTER URL any vedio Yuotube=======>")
 vid =new(vido)
 down = vid.getbest()
